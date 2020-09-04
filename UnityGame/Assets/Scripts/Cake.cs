57bd75b9bceea541ba053b93f1f75d0caea918f6
Katzesama <31713290+Katzesama@users.noreply.github.com>
2020-09-03 17:40:39 -0700

    improve level
    added jump sound and eat cake sound, and added cakes to the level

---
+++ UnityGame/Assets/Scripts/Cake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		// infinitely rotate this coin about the Y axis in world space
+		transform.Rotate(0, 5f, 0, Space.World);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		// trigger coin pickup function if a helicopter collides with this
+		GameObject.FindObjectOfType<SlimeController>().EatCake();
+		GameObject.Find("CakeEatten").GetComponent<AudioSource>().Play();
+		Destroy(gameObject);
+	}
+}
