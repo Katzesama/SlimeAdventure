22641eee4c5c68ec57c188001bc64b354dd175ed
Katzesama <31713290+Katzesama@users.noreply.github.com>
2020-09-12 13:52:00 -0700

    done
    added menu scene, end scene

---
+++ UnityGame/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
